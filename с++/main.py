f1688544402a36f35214ec2b45b11ec2da335281
4zett <list_evreya@mail.ru>
2022-02-11 23:25:17 +0400

    c++
    c++

---
+++ с++/main.py
@@ -0,0 +1,8 @@
+n = 10
+a = [1, -4, 12, 547, -348, 363, 210, -10, 637, -127]
+for i in range(1, n-1):
+    if a[i] >= 0:
+        print(a[i])
+for i in range(1, n-1):
+    if a[i] < 0:
+        print(a[i])
