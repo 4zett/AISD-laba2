3bdc470ec1017503f09aa87d5419ccff140d6e7a
4zett <99506244+4zett@users.noreply.github.com>
2022-04-26 17:04:09 +0400

    Delete с++ directory

--- с++/main.py
+++
@@ -1,8 +0,0 @@
-n = 10
-a = [1, -4, 12, 547, -348, 363, 210, -10, 637, -127]
-for i in range(1, n-1):
-    if a[i] >= 0:
-        print(a[i])
-for i in range(1, n-1):
-    if a[i] < 0:
-        print(a[i])
