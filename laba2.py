38e454624ce29679e4c88ec44973f21e0106a2c2
4zett <99506244+4zett@users.noreply.github.com>
2022-05-11 15:33:05 +0400

    Add files via upload

--- laba2.py
+++ laba2.py
@@ -1,14 +1,33 @@
 import re
+work_buffer = ''
+fin_buffer = ''
+buffer_len = 1
+flag = False
+flag2 = False
+h = 0
 try:
     print("\n ___  Результат работы программы  ___ \n")
     with open('laboratornaya2.txt', 'r') as file:
-        buffer = file.read()
+        buffer = file.read(buffer_len)
         if not buffer:
             print('\nРабочий файл пустой, измените содержание файла')
-        if re.findall(r'"[\w*\s]{0,}\S"', buffer):
-            work_buffer = re.findall(r'"[\w*\s]{0,}\S"', buffer)
-            print(work_buffer)
-        else:
-            print('В файле laboratornaya2.txt отсутствуют цитаты.\nОтредактируйте файл и запустите программу заново.')
+        while buffer:
+            if re.findall(r'["]', buffer) and not flag2:
+                work_buffer += buffer
+                buffer = file.read(buffer_len)
+                flag = True
+                flag2 = True
+                h += 1
+            if re.findall(r'[\w\s\S,;:^"]', buffer) and flag:
+                work_buffer += buffer
+            if re.findall(r'["]', buffer) and flag:
+                work_buffer += buffer
+                print(work_buffer[:-1])
+                work_buffer = ""
+                flag = False
+                flag2 = False
+            buffer = file.read(buffer_len)
+        if h == 0 and buffer:
+            print('\nВ тексте отсутствуют цитаты.')
 except FileNotFoundError:
     print('\nФайл laboratornaya2.txt в директории проекта не обнаружен.\nДобавьте файл в директорию или переименуйте существующий')
